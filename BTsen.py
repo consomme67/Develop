023afaa1a4044c0ac8e52bf2f6e15b939a0533d3
consomme67 <zfg3ki006@gmail.com>
2021-02-26 19:24:44 +0900

    02/26最終

--- BTsen.py
+++ BTsen.py
@@ -17,31 +17,31 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_PIN, GPIO.IN)
 
 def kokoha_GET():
-	"""鯖からとってきてキュー作ってそこにＩＤとtimestamp放り込む"""
-	return
+    """鯖からとってきてキュー作ってそこにＩＤとtimestamp放り込む"""
+    return
 def kinjiti_out():
-	"""absでキュー内のtimestampと↓のcentralの中との近似値とる"""
-	return
+    """absでキュー内のtimestampと↓のcentralの中との近似値とる"""
+    return
 
 
 
 def count_sen():
-	while True:
-		# センサー感知
-		if (GPIO.input(GPIO_PIN) == GPIO.HIGH):
-			nowmil = str(int(time.time() * 1000))
-			with open("/home/pi/doc/central.txt", mode='a') as c:
-				c.write(nowmil+'\n')
-			time.sleep(SLEEPTIME)
-		else:
-			time.sleep(INTERVAL)
+    try:
+        while True:
+            # センサー感知
+            if (GPIO.input(GPIO_PIN) == GPIO.HIGH):
+                nowmil = str(int(time.time() * 1000))
+                with open("/home/pi/doc/central.txt", mode='a') as c:
+                    c.write(nowmil+'\n')
+                time.sleep(SLEEPTIME)
+            else:
+                time.sleep(INTERVAL)
 
-GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
-	print("===START===")
-
-
-	time.sleep(3)
-	count_sen()
+    print("===START===")
+    time.sleep(3)
+    count_sen()
