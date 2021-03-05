cba0585803ac778d7108ea3711e4646f2f295b8c
consomme67 <zfg3ki006@gmail.com>
2021-03-05 18:57:12 +0900

    aaaaaaaa

---
+++ test.py
@@ -0,0 +1,12 @@
+
+import subprocess
+
+def main():
+    command00 = f'ffmpeg -f image2 -r 15 -i pic/%01d.jpg -r 15 -an -vcodec libx264 -pix_fmt yuv420p pic/video/video.mp4'
+                f'ffmpeg -seek_timestamp 1 -ss {centime - 5000} -i {input_path} -t 5 -framerate 30\
+                    -c:v copy -y /home/pi/doc/mov/cam{cam_num}.mkv'
+
+    subprocess.call(command00, shell=True)
+
+if __name__ == '__main__':
+    main()
